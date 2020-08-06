d073e1b1a1ccdf5529799d0bc9fcb93be63c99d3
Hashiyaa <haochen213300@gmail.com>
2020-08-06 15:41:28 -0500

    Update the wrapper script

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,4 @@ pub mod utils_rust;
 pub mod spacetime;
 pub mod groove;
 pub mod relaxed_ik;
-pub mod relaxed_ik_ros1;
-pub mod relaxed_ik_ros2;
-pub mod relaxed_ik_unity;
-pub mod relaxed_ik_coppeliasim;
+pub mod relaxed_ik_wrapper;
--- src/relaxed_ik_wrapper.rs
+++ src/relaxed_ik_wrapper.rs
@@ -7,10 +7,10 @@ use std::os::raw::{c_double, c_int};
 use std::cell::RefCell;
 
 #[no_mangle]
-pub unsafe extern "C" fn run_unity(pos_arr: *const c_double, pos_length: c_int, 
+pub unsafe extern "C" fn solve(pos_arr: *const c_double, pos_length: c_int, 
     quat_arr: *const c_double, quat_length: c_int) -> relaxed_ik::Opt {
-    assert!(!pos_arr.is_null(), "Null pointer for pos goals");
-    assert!(!quat_arr.is_null(), "Null pointer for quat goals");
+    assert!(!pos_arr.is_null(), "Null pointer for pos goals!");
+    assert!(!quat_arr.is_null(), "Null pointer for quat goals!");
 
     let pos_slice: &[c_double] = std::slice::from_raw_parts(pos_arr, pos_length as usize);
     let quat_slice: &[c_double] = std::slice::from_raw_parts(quat_arr, quat_length as usize);
@@ -18,7 +18,7 @@ pub unsafe extern "C" fn run_unity(pos_arr: *const c_double, pos_length: c_int,
     let pos_vec = pos_slice.to_vec();
     let quat_vec = quat_slice.to_vec();
 
-    let ja = run(pos_vec, quat_vec);
+    let ja = solve_helper(pos_vec, quat_vec);
     let ptr = ja.as_ptr();
     let len = ja.len();
     std::mem::forget(ja);
@@ -27,7 +27,7 @@ pub unsafe extern "C" fn run_unity(pos_arr: *const c_double, pos_length: c_int,
 
 thread_local!(static R: RefCell<relaxed_ik::RelaxedIK> = RefCell::new(relaxed_ik::RelaxedIK::from_loaded(1)));
 
-fn run(pos_goals: Vec<f64>, quat_goals: Vec<f64>) -> Vec<f64> {
+fn solve_helper(pos_goals: Vec<f64>, quat_goals: Vec<f64>) -> Vec<f64> {
     let mut x: Vec<f64> = Vec::new();
     let arc = Arc::new(Mutex::new(EEPoseGoalsSubscriber::new()));
     let mut g = arc.lock().unwrap();
@@ -35,10 +35,9 @@ fn run(pos_goals: Vec<f64>, quat_goals: Vec<f64>) -> Vec<f64> {
     R.with(|r| {
         for i in 0..(*r.borrow()).vars.robot.num_chains {
             g.pos_goals.push( Vector3::new(pos_goals[3*i], pos_goals[3*i+1], pos_goals[3*i+2]) );
-            let tmp_q = Quaternion::new(quat_goals[3*i+3], quat_goals[3*i], quat_goals[3*i+1], quat_goals[3*i+2]);
+            let tmp_q = Quaternion::new(quat_goals[3*i], quat_goals[3*i+1], quat_goals[3*i+2], quat_goals[3*i+3]);
             g.quat_goals.push( UnitQuaternion::from_quaternion(tmp_q) );
         }
-        // println!("pos: {:?}, quat: {:?}", g.pos_goals, g.quat_goals);
     
         x = (*r.borrow_mut()).solve(&g);
         // println!("{:?}", x);
